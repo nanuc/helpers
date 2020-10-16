f75e65b14547056fa3999060d46f5d02ab1c4302
Sebastian Schöps <ssc@schoeps-consult.de>
2020-10-16 13:24:39 +0200

    Use user information in Helpscout Beacon

--- config/helpers.php
+++ config/helpers.php
@@ -3,7 +3,8 @@
 return [
     'helpscout' => [
         'beacon' => [
-            'key' => env('HELPSCOUT_BEACON_KEY')
+            'key' => env('HELPSCOUT_BEACON_KEY'),
+            'identify-user' => env('HELPSCOUT_BEACON_IDENTIFY_USER', true)
         ]
     ]
 ];
