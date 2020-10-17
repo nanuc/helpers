fe3d6d967a542f18782f48e874f0fdfdb7cb3c5f
Sebastian Schöps <ssc@schoeps-consult.de>
2020-10-18 00:14:47 +0200

    Add quick log method

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,13 @@
 <?php
+
+if (! function_exists('l')) {
+    /**
+     * Quick log.
+     *
+     * @param  mixed $string
+     */
+    function l($string)
+    {
+        \Illuminate\Support\Facades\Log::info($string);
+    }
+}
