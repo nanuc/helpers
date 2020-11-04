30115cb2b8c4a11444e869b498e2b18ab6ee081e
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2020-11-04 20:31:39 +0100

    Add userHasTeamRole helper

--- src/helpers.php
+++ src/helpers.php
@@ -24,3 +24,24 @@ if (! function_exists('currentTeam')) {
         return optional(auth()->user())->currentTeam;
     }
 }
+
+if (! function_exists('userHasTeamRole')) {
+    /**
+     * Check if the current user has a given role in the current team.
+     *
+     * @return boolean
+     *
+     */
+    function userHasTeamRole($roles)
+    {
+        if($team = currentTeam()) {
+            foreach(\Illuminate\Support\Arr::wrap($roles) as $role) {
+                if(auth()->user()->hasTeamRole($team, $role)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
