8c32e8e937756b152673f4d1125093de65a7f313
Sebastian Schöps <ssc@schoeps-consult.de>
2020-11-19 17:53:57 +0100

    Add datetime helper

--- src/helpers.php
+++ src/helpers.php
@@ -11,3 +11,20 @@ if (! function_exists('l')) {
         \Illuminate\Support\Facades\Log::info($string);
     }
 }
+
+if (! function_exists('datetime')) {
+    /**
+     * Format a date.
+     *
+     * @param $format
+     * @param string $date
+     * @param bool $entitites
+     * @return bool|string
+     */
+    function datetime($format, $date = 'now', $entitites = true)
+    {
+        $datetimeString = utf8_encode(strftime(trans('datetime.locale.'.$format), strtotime($date)));
+
+        return $entitites ? htmlentities($datetimeString) : $datetimeString;
+    }
+}
