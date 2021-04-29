e6b3b1f101ad772c122bbae83ccbf4513531daa8
Sebastian Schöps <ssc@schoeps-consult.de>
2021-04-29 02:11:42 +0200

    Fix purging

--- resources/views/components/buttons/social-login.blade.php
+++ resources/views/components/buttons/social-login.blade.php
@@ -5,7 +5,7 @@ $color = [
 ][$provider];
 ?>
 <a href="{{ route('socialite.redirect', ['provider' => $provider]) }}" class="w-full block cursor-pointer">
-    <div class="mt-2 w-full inline-flex items-center px-4 py-2 bg-{{ $color }}-800 border border-transparent rounded-md font-semibold text-white uppercase tracking-widest hover:bg-{{ $color }}-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+    <div class="mt-2 w-full inline-flex items-center px-4 py-2 bg-{{ $color }}-800 border border-transparent rounded-md font-semibold text-white uppercase tracking-widest hover:bg-{{ $color }}-700 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
         <x-dynamic-component :component="'si-' . $provider" class="w-10 mr-4" />
         {{ __('Login with :provider', ['provider' => $provider]) }}
     </div>
--- resources/views/components/purge-styles.blade.php
+++ resources/views/components/purge-styles.blade.php
@@ -1,1 +1,1 @@
-<div class="bg-red-700 bg-red-800 bg-blue-700 bg-blue-800"></div>
+<div class="hover:bg-red-700 bg-red-800 hover:bg-blue-700 bg-blue-800"></div>
