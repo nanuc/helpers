19b81107dec82537fda423c40f5a1509766eae4a
Sebastian Schöps <44115562+SebastianSchoeps@users.noreply.github.com>
2021-10-20 23:52:43 +0200

    Update loading-spinner.blade.php

--- resources/views/components/loading-spinner.blade.php
+++ resources/views/components/loading-spinner.blade.php
@@ -1,1 +1,4 @@
-<img class="h-{{ $height ?? 10 }}" id="{{ $id ?? '' }}" src="{{ asset('/images/loading.gif') }}">
+<img class="inline-block h-{{ $height ?? 10 }}" id="{{ $id ?? '' }}" src="{{ asset('/images/loading.gif') }}">
+@if(isset($label))
+    <div class="inline-block h-{{ $height ?? 10 }} text-gray-600">{{ $label }}</div>
+@endif
