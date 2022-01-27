001f940e147265abf01633b22742e4e90ca391f8
joschua-nanuc <64951248+joschua-nanuc@users.noreply.github.com>
2022-01-27 10:15:25 +0100

    Create disabled-button.blade.php

---
+++ resources/views/components/buttons/disabled-button.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes }} class="inline-flex items-center justify-center font-semibold tracking-tight rounded-lg focus:outline-none h-9 px-4 text-gray-400 bg-white border border-gray-300">
+    <span>{{ $slot }}</span>
+</div>
