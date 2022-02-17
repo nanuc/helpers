eb8de28ac92567b1905733ec6c8f339103b4cdd3
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2022-02-17 13:21:00 +0100

    New modals

---
+++ resources/views/components/modals/confirm.blade.php
@@ -0,0 +1,31 @@
+@props(['size' => 'lg', 'caption', 'heading', 'trigger', 'action', 'confirmButtonText' => __('Absolutely!')])
+
+<x-app-ui::modal>
+    <x-slot name="trigger">
+        @if(isset($trigger))
+            {{ $trigger }}
+        @else
+            <x-app-ui::link x-on:click="open = true" class="cursor-pointer">
+                {{ $caption }}
+            </x-app-ui::link>
+        @endif
+    </x-slot>
+
+    <x-slot name="heading">
+        {{ $caption }}
+    </x-slot>
+
+    {{ $slot }}
+
+    <x-slot name="footer">
+        <x-app-ui::modal.actions full-width>
+            <x-app-ui::button x-on:click="open = false" color="secondary">
+                {{ __('Cancel') }}
+            </x-app-ui::button>
+
+            <x-app-ui::button color="primary" x-on:click="open = false" wire:click="{{ $action }}">
+                {{ $confirmButtonText }}
+            </x-app-ui::button>
+        </x-app-ui::modal.actions>
+    </x-slot>
+</x-app-ui::modal>
---
+++ resources/views/components/modals/general.blade.php
@@ -0,0 +1,29 @@
+@props(['size' => 'lg', 'caption', 'heading', 'buttonSize' => 'md', 'actionButtonText' => null, 'action', 'closeButtonText' => __('Close')])
+
+<x-app-ui::modal>
+    <x-slot name="trigger">
+        <x-app-ui::button x-on:click="open = true" :size="$buttonSize" color="secondary">
+            {{ $caption }}
+        </x-app-ui::button>
+    </x-slot>
+
+    <x-slot name="heading">
+        {{ $heading ?? $caption }}
+    </x-slot>
+
+    {{ $slot }}
+
+    <x-slot name="footer">
+        <x-app-ui::modal.actions full-width>
+            <x-app-ui::button x-on:click="open = false" color="secondary">
+                {{ $closeButtonText }}
+            </x-app-ui::button>
+
+            @if($actionButtonText)
+                <x-app-ui::button color="primary" x-on:click="open = false" wire:click="{{ $action }}">
+                    {{ $actionButtonText }}
+                </x-app-ui::button>
+            @endif
+        </x-app-ui::modal.actions>
+    </x-slot>
+</x-app-ui::modal>
