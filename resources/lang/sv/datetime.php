9e01b859d59210518d2863008e84496661fc9776
Sebastian Schöps <44115562+SebastianSchoeps@users.noreply.github.com>
2023-03-22 10:41:25 +0100

    Create datetime.php

---
+++ resources/lang/sv/datetime.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'medium' => '%a, %b %d, %Y, %I:%M%p',
+    'long' => '%A, %B %d, %Y, %I:%M%p',
+    'short' => '%m/%d %Y, %I:%M%p',
+    'with-seconds' => '%m/%d/%Y, %H:%M:%S',
+    'short-without-year' => '%B %d, %I:%M%p',
+    'short-with-weekday-without-year' => '%A, %B %d, %I:%M%p',
+    'date' => [
+        'short' => '%m/%d/%Y',
+        'medium' => '%A, %B %d, %Y',
+        'short-without-year' => '%m/%d',
+        'short-with-weekday-without-year' => '%A, %B %d',
+    ],
+    'time' => [
+        'short' => '%I:%M%p',
+        'short-appendix' => '%I:%M%p',
+        'with-weekday' => '%A, %I:%M%p',
+    ],
+];
