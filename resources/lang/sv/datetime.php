3ccf8c234abdee656c6e2daba0fd17cb2187c7a9
Sebastian Schöps <44115562+SebastianSchoeps@users.noreply.github.com>
2023-03-22 13:12:04 +0100

    Update datetime.php

--- resources/lang/sv/datetime.php
+++ resources/lang/sv/datetime.php
@@ -3,14 +3,14 @@
 return [
     'medium' => '%a, %b %d, %Y, %I:%M%p',
     'long' => '%A, %B %d, %Y, %I:%M%p',
-    'short' => '%m/%d %Y, %I:%M%p',
-    'with-seconds' => '%m/%d/%Y, %H:%M:%S',
+    'short' => '%d/%m %Y, %I:%M%p',
+    'with-seconds' => '%d/%m/%Y, %H:%M:%S',
     'short-without-year' => '%B %d, %I:%M%p',
     'short-with-weekday-without-year' => '%A, %B %d, %I:%M%p',
     'date' => [
-        'short' => '%m/%d/%Y',
+        'short' => '%d/%m/%Y',
         'medium' => '%A, %B %d, %Y',
-        'short-without-year' => '%m/%d',
+        'short-without-year' => '%d/%m',
         'short-with-weekday-without-year' => '%A, %B %d',
     ],
     'time' => [
